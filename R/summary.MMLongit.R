a83459026fdaf59f8ee6cd8a85e3d43a232e6691
Nate Mercaldo <nate.mercaldo@vanderbilt.edu>
2017-07-11 12:24:00 -0500

    Modifying summary function to print robust se when using WL

--- R/summary.MMLongit.R
+++ R/summary.MMLongit.R
@@ -6,12 +6,12 @@ function(object, ...) {
   mean.oo <- with(oo, data.frame(estimate = beta, mod.se = sqrt(diag(mod.cov)[m.i])))
   mean.oo$X2  <- with(mean.oo, (estimate/mod.se)^2)
   mean.oo$pX2 <- with(mean.oo, pchisq(X2, df=1,lower.tail=FALSE))
-  names(mean.oo) = c('Estimate','Mod.SE', 'Chi Square','Pr(>Chi)')
+  names(mean.oo) = c('Estimate','Model SE', 'Chi Square','Pr(>Chi)')
   
   assoc.oo <- with(oo, data.frame(estimate = alpha, mod.se = sqrt(diag(mod.cov)[a.i])))
   assoc.oo$X2  <- with(assoc.oo, (estimate/mod.se)^2)
   assoc.oo$pX2 <- with(assoc.oo, pchisq(X2, df=1,lower.tail=FALSE))
-  names(assoc.oo) = c('Estimate','Mod.SE','Chi Square','Pr(>Chi)')
+  names(assoc.oo) = c('Estimate','Model SE','Chi Square','Pr(>Chi)')
   
   out = list(class = class(object), call = deparse(object$call), control=object$control, 
              info=object$info_stats, mean.table = mean.oo, assoc.table=assoc.oo)
