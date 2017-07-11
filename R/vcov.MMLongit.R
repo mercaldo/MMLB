486b40484e40d96210e8915cde8feeb2b668dcaf
Nate Mercaldo <nate.mercaldo@vanderbilt.edu>
2017-07-11 14:14:49 -0500

    Updated vcov for weighted analysis

--- R/vcov.MMLongit.R
+++ R/vcov.MMLongit.R
@@ -1,9 +1,9 @@
-vcov.MMLongit <- function (object, robust=FALSE) {
+vcov.MMLongit <- function (object) {
   n_beta     = length(object$beta)
   n_alpha    = length(object$alpha)
   beta_ix    = seq(n_beta)
   alpha_ix   = seq(n_beta+1, n_beta+n_alpha)
   object_vc  = object$mod.cov
-  if(robust) object_vc  = object$emp.cov
+  if(object$control[7]) object_vc  = object$rob.cov
   list('beta'=object_vc[beta_ix,beta_ix], 'alpha'=object_vc[alpha_ix,alpha_ix])
 }
